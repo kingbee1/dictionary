9edb07cd59a7c1fa2e63330d80b80679491152e8
Abiodun <abiodunhodonu@gmail.com>
2023-07-11 17:56:00 +0100

    fetched and mapped through data

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,22 +1,34 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Employee = () => {
+  const [worker, setWorker] = useState([])
+
+
+  useEffect(()=>{
+    getEmployee()
+  }, [])
+
+  const getEmployee = async () => {
+    const api = await fetch ('https://jsonplaceholder.typicode.com/photos')
+    const data = await api.json()
+    setWorker(data)
+    console.log(data)
+  }
+
   return (
-    <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-      <img
-        className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
-        src="/img/erin-lindford.jpg"
-        alt="Woman's Face"
-      />
-      <div className="text-center space-y-2 sm:text-left">
-        <div className="space-y-0.5">
-          <p className="text-lg text-black font-semibold">Erin Lindford</p>
-          <p className="text-slate-500 font-medium">Product Engineer</p>
+    <div>
+     <h1>All employees</h1>
+     {worker.map((item, id)=> {
+
+      return (
+        <div key={id}>
+          <p>{item.title}</p>
         </div>
-        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-          Message
-        </button>
-      </div>
+      )
+     })}
+     
     </div>
   );
 };
