69b054b1c1671b763c085abd58199adb8409fa79
Abiodun <abiodunhodonu@gmail.com>
2023-07-12 15:41:49 +0100

    fixed search issues

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -5,38 +5,48 @@ import { useEffect } from "react";
 const Employee = () => {
   const [worker, setWorker] = useState([])
   const [search, setSearch] = useState('')
+  const [endPoint, setEndPoint] = useState('')
 
 
   useEffect(()=>{
-    getEmployee()
-  }, [])
+    fetchMe()
+  }, [endPoint])
 
-  const getEmployee = async () => {
-    const api = await fetch ('https://jsonplaceholder.typicode.com/photos')
+  const fetchMe = async () => {
+    const api = await fetch (`https://jsonplaceholder.typicode.com/photos/${search}`)
+    console.log({search})
     const data = await api.json()
-    setWorker(data)
     console.log({data})
+    setWorker(data instanceof Array ? data : [data])
+    //console.log({data})
     //console.log(data[0].title)
+
   }
 
   const onChangeHandler = (e) => {
     setSearch(e.target.value)
   }
 
+//to submit form/search obviously.
+const submitHandler = e => {
+  e.preventDefault()
+  setEndPoint(search)
+}
+
   return (
     <div>
-      <form>
+      <form onSubmit={submitHandler}>
         <input type="text" value={search} onChange={onChangeHandler} />
         <button type="submit" placeholder="search employee">search</button>
       </form>
      <h1>All employees</h1>
-     {worker.map((data, id)=> {
+     {worker.map((item, id)=> {
 
       return (
         <div key={id}>
-          <h1>{data.id}</h1>
+          <h1>{item.id}</h1>
           <h4></h4>
-          <p>{data.url}</p>
+          <p>{item.url}</p>
         </div>
       )
      })}
